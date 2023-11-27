0ec928ce66ffd98364fb72c9ead7a170d1d41f94
Elvis Garcia <elviserikvan@gmail.com>
2023-11-26 23:19:14 -0400

    feat: Order Model Created
    I've created the Order model with its migration and its relationship
    with the Client model

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Address;
+use App\Models\Order;
 
 class Client extends Model
 {
@@ -19,5 +20,9 @@ class Client extends Model
 
     public function addresses() {
         return $this->hasMany(Address::class);
+    }
+
+    public function orders() {
+        return $this->hasMany(Order::class);
     }
 }
